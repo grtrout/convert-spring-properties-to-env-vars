74cf63d599cbadbe0204100c499167f307fa1463
Greg Trout <gregory.trout@fedex.com>
2021-10-22 08:47:03 -0400

    Refactored script to improve readability

--- convert-spring-properties-to-env-vars.sh
+++ convert-spring-properties-to-env-vars.sh
@@ -3,34 +3,35 @@
 # Greg Trout | grtrout@gmail.com
 
 convert_key() {
-  local UPPERCASED=$(echo ${1} | tr "[:lower:]" "[:upper:]")
-  local DASHES_REMOVED=$(echo "${UPPERCASED//-/}")
-  KEY="${DASHES_REMOVED//[^[:alnum:]]/_}" # Convert non-alphanumeric chars to underscores
+  # Capitalize key and remove dashes
+  local NEW_KEY="$(echo ${1} | tr "[:lower:]" "[:upper:]")"
+  NEW_KEY="$(echo "${NEW_KEY//-/}")"
+  KEY="${NEW_KEY//[^[:alnum:]]/_}" # Convert non-alphanumeric chars to underscores
 }
 
 convert_value() {
   # Trim leading and trailing spaces
-  local TRIMMED_LEADING="${1#"${1%%[![:space:]]*}"}"
-  local TRIMMED_TRAILING="${TRIMMED_LEADING%"${TRIMMED_LEADING##*[![:space:]]}"}"
+  local NEW_VALUE="${1#"${1%%[![:space:]]*}"}"
+  NEW_VALUE="${NEW_VALUE%"${NEW_VALUE##*[![:space:]]}"}"
 
   # Determine first and last char of value
-  local FIRST_CHAR=${TRIMMED_TRAILING:0:1}
-  local LAST_CHAR=${TRIMMED_TRAILING: -1}
+  local FIRST_CHAR="${NEW_VALUE:0:1}"
+  local LAST_CHAR="${NEW_VALUE: -1}"
 
   # Only add double quotes around value if it's not already surrounded by single or double quotes
   if ([[ ${FIRST_CHAR} = ${LAST_CHAR} ]]) && ([[ ${FIRST_CHAR} = \' || ${FIRST_CHAR} = \" ]]); then
-    VALUE="${TRIMMED_TRAILING}"
+    VALUE="${NEW_VALUE}"
   else
-    VALUE="\"${TRIMMED_TRAILING}\"" 
+    VALUE="\"${NEW_VALUE}\""
   fi
 }
 
 # Loop through each line of input file
-while IFS='' read -r LINE || [ -n "${LINE}" ]; do
-  [[ "${LINE}" =~ ^[[:space:]]*# ]] && continue # Ignore comments
-  if [ -n "${LINE}" ]; then
-    KEY=${LINE%=*}   # Delete from = to the right
-    VALUE=${LINE#*=} # Delete up to =
+while IFS='' read -r LINE || [[ -n ${LINE} ]]; do
+  [[ ${LINE} =~ ^[[:space:]]*# ]] && continue # Ignore comments
+  if [[ -n ${LINE} ]]; then
+    KEY="${LINE%=*}"   # Delete from = to the right
+    VALUE="${LINE#*=}" # Delete up to =
     convert_key ${KEY}
     convert_value ${VALUE}
     echo "${KEY}: ${VALUE}" # Echo output to stdout
