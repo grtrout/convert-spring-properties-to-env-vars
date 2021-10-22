eee90bcb273d7bd8168b9e0893e5276020360c16
Greg Trout <gregory.trout@fedex.com>
2021-10-22 09:21:27 -0400

    Fixed parameterization issue that caused problems with wildcard (*) values

--- convert-spring-properties-to-env-vars.sh
+++ convert-spring-properties-to-env-vars.sh
@@ -3,37 +3,32 @@
 # Greg Trout | grtrout@gmail.com
 
 convert_key() {
-  # Capitalize key and remove dashes
-  local NEW_KEY="$(echo ${1} | tr "[:lower:]" "[:upper:]")"
-  NEW_KEY="$(echo "${NEW_KEY//-/}")"
-  KEY="${NEW_KEY//[^[:alnum:]]/_}" # Convert non-alphanumeric chars to underscores
+  local NEW_KEY="$(echo ${1} | tr "[:lower:]" "[:upper:]")" # Convert to uppercase
+  NEW_KEY="$(echo "${NEW_KEY//-/}")" # Remove dashes
+  KEY="${NEW_KEY//[^[:alnum:]]/_}"   # Convert other non-alphanumeric chars to underscores
 }
 
 convert_value() {
-  # Trim leading and trailing spaces
-  local NEW_VALUE="${1#"${1%%[![:space:]]*}"}"
-  NEW_VALUE="${NEW_VALUE%"${NEW_VALUE##*[![:space:]]}"}"
+  local NEW_VALUE="${1#"${1%%[![:space:]]*}"}"           # Trim leading spaces
+  NEW_VALUE="${NEW_VALUE%"${NEW_VALUE##*[![:space:]]}"}" # Trim trailing spaces
 
-  # Determine first and last char of value
-  local FIRST_CHAR="${NEW_VALUE:0:1}"
-  local LAST_CHAR="${NEW_VALUE: -1}"
+  local FIRST_CHAR="${NEW_VALUE:0:1}" # Determine first character
+  local LAST_CHAR="${NEW_VALUE: -1}"  # Determine last character
 
-  # Only add double quotes around value if it's not already surrounded by single or double quotes
   if ([[ ${FIRST_CHAR} = ${LAST_CHAR} ]]) && ([[ ${FIRST_CHAR} = \' || ${FIRST_CHAR} = \" ]]); then
-    VALUE="${NEW_VALUE}"
+    VALUE="${NEW_VALUE}" # Variable already surrounded with single or double quotes; leave as-is
   else
-    VALUE="\"${NEW_VALUE}\""
+    VALUE="\"${NEW_VALUE}\"" # Surround variable with double quotes
   fi
 }
 
-# Loop through each line of input file
-while IFS='' read -r LINE || [[ -n ${LINE} ]]; do
-  [[ ${LINE} =~ ^[[:space:]]*# ]] && continue # Ignore comments
+while IFS='' read -r LINE || [[ -n ${LINE} ]]; do # Loop through each line of input file
+  [[ ${LINE} =~ ^[[:space:]]*# ]] && continue     # Ignore comments
   if [[ -n ${LINE} ]]; then
     KEY="${LINE%=*}"   # Delete from = to the right
     VALUE="${LINE#*=}" # Delete up to =
-    convert_key ${KEY}
-    convert_value ${VALUE}
-    echo "${KEY}: ${VALUE}" # Echo output to stdout
+    convert_key "${KEY}"
+    convert_value "${VALUE}"
+    echo "${KEY}: ${VALUE}" # Send output to stdout
   fi
 done <${1}
